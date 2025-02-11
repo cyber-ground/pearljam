2e910ddba90566b3c2ac1d571ff4b28bc3140cb8
cyber-ground <81938276+cyber-ground@users.noreply.github.com>
2025-02-11 15:03:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,89 @@
 'use strict';
 
+import {console_color,console_red,console_orange,console_yellow,console_green,
+  console_blue,console_purple,console_magenta,console_cyan} from './logColor.js';
 
-
-//---------------------------------------------------------------------------------------------------
-
-//                                     ----- PEARL JAM -----
-//---------------------------------------------------------------------------------------------------
-
+//----------------------------------------------------------------------------------------------
+//*                               ----- DIZZY MONSTER -----
+//----------------------------------------------------------------------------------------------
 
 
 const startBtn = document.querySelector('.start-btn'); 
 const gameClearMessage = document.querySelector('.game-clear');
 const gameOverMessage = document.querySelector('.game-over');
+const container = document.querySelector('.container');
 const cards = document.querySelectorAll('.memory-card');
+const mobile = navigator.userAgent.match(/iPhone|Android.+Mobile/);
+let portrait = window.matchMedia('(orientation: portrait)').matches;
 let hasFlippedCard, lockBoard; 
 let firstCard, secondCard;
 let matched,unMatched;
+let touch = false;
+let startGame = false;
+let defaultHeight = innerHeight;
+let orientation = portrait ? 'portrait' : 'landscape';
+let lastOrientation = orientation;
+let menubar;
+var bgmHowl = new Howl({src: ['mp3/bgm.mp3'], loop:true, volume: 0.05});
+var flipCardHowl = new Howl({src: ['mp3/flipCard.mp3'], volume: 0.5});
+var unmatchedHowl = new Howl({src: ['mp3/unmatched.mp3'], volume: 0.5});
+var matchedHowl = new Howl({src: ['mp3/matched.mp3'], volume: 0.5});
+var gameClearHowl = new Howl({src: ['mp3/gameClear.mp3'], volume: 0.1});
+var gameOverHowl = new Howl({src: ['mp3/gameOver.mp3'], volume: 0.1});
 
-  var bgmHowl = new Howl({src: ['mp3/bgm.mp3'], loop:true, volume: 0.05});
-  var flipCardHowl = new Howl({src: ['mp3/flipCard.mp3'], volume: 0.5});
-  var unmatchedHowl = new Howl({src: ['mp3/unmatched.mp3'], volume: 0.5});
-  var matchedHowl = new Howl({src: ['mp3/matched.mp3'], volume: 0.5});
-  var gameClearHowl = new Howl({src: ['mp3/gameClear.mp3'], volume: 0.3});
-  var overHowl = new Howl({src: ['mp3/over.mp3'], volume: 0.3});
-  var gameOverHowl = new Howl({src: ['mp3/gameOver.mp3'], volume: 0.3});
-  
-  startBtn.classList.add('js_visible'); //*>
-  startBtn.addEventListener('click', function () {
-    cards.forEach(card => {
-      card.classList.remove('js_flip');
-      card.addEventListener('click', flipCard);
-    });
-    startBtn.classList.remove('js_visible');
-    gameClearMessage.classList.remove('js_visible');
-    gameOverMessage.classList.remove('js_visible');
-    startBtn.classList.add('active');
-    shuffleCards();
-    hasFlippedCard = false; 
-    lockBoard = false;
-    matched = 0;
-    unMatched = 0;
-    bgmHowl.play();
+//* touchCalloutPreventionEvents ---
+container.addEventListener('touchstart', e => e.preventDefault());
+cards.forEach(card => {
+  card.addEventListener('touchstart', e => e.preventDefault());
+});
+gameClearMessage.addEventListener('touchstart', e => e.preventDefault());
+gameOverMessage.addEventListener('touchstart', e => e.preventDefault());
+
+startBtn.addEventListener('touchstart', (e) => {
+  e.preventDefault();
+  startBtn.click();
+});
+
+  const imgs = document.querySelectorAll('img');
+imgs.forEach(img => {
+  img.addEventListener('touchstart', (e) => {
+    if(startGame) {
+      if(!touch) { touch = true; e.stopPropagation()}
+    }
   });
+  img.addEventListener('mousedown', () => {
+    img.style.pointerEvents = 'none';
+    setTimeout(() => { img.style.pointerEvents = 'all'}, 500);
+  });
+  img.addEventListener('touchend', () => {
+    setTimeout(() => { touch = false}, 150);
+  });
+});
 
+//* ------------------------------------
+
+  startBtn.classList.add('js_visible'); //*>
+startBtn.addEventListener('click', function () {
+  cards.forEach(card => {
+    card.classList.remove('js_flip');
+    card.addEventListener('click', flipCard);
+  });
+  startBtn.classList.remove('js_visible');
+  gameClearMessage.classList.remove('js_visible');
+  gameOverMessage.classList.remove('js_visible');
+  startBtn.classList.add('active');
+  shuffleCards();
+  hasFlippedCard = false; 
+  lockBoard = false;
+  matched = 0;
+  unMatched = 0;
+  bgmHowl.play();
+  startGame = true; //*
+});
 
 function flipCard() {
-    if(lockBoard) return;            
-    if(this === firstCard) return;     
+  if(lockBoard) return;            
+  if(this === firstCard) return;     
   this.classList.add('js_flip');
   flipCardHowl.play();
   if(!hasFlippedCard) {
@@ -74,7 +111,7 @@ function matchedCards() {
 }
 
 function unMatchedCards() {
-  lockBoard = true;
+  lockBoard = true; 
   setTimeout(() => {
     firstCard.classList.remove('js_flip');
     secondCard.classList.remove('js_flip');
@@ -83,45 +120,47 @@ function unMatchedCards() {
     firstCard = null;
     unMatched++; 
     gameOverCounter();
-  }, 1000); 
+  }, 1000);  
 }
 
 function gameClear() {
-  if(matched === cards.length / 2) {  
-    setTimeout(() => {               
+  if(matched === cards.length / 2) {
+    setTimeout(() => {
       gameClearMessage.classList.add('js_visible');
       disableCards();
       bgmHowl.stop();
+      startGame = false;
+      firstCard = null;
       setTimeout(() => { gameClearHowl.play()}, 800);
-      setTimeout(() => { 
+      setTimeout(() => {
         startBtn.classList.add('js_visible');
       }, 3000);
     }, 500);
   }
 }
 
-function gameOver() {                                  
-  gameOverMessage.classList.add('js_visible');                         
-  disableCards();     
+function gameOver() {
+  gameOverMessage.classList.add('js_visible');
+  disableCards();
   bgmHowl.stop();
-  setTimeout(() => { overHowl.play()}, 500); 
-  setTimeout(() => { gameOverHowl.play()}, 2800);
+  gameOverHowl.play();
+  startGame = false;
   setTimeout(() => {
-    startBtn.classList.add('js_visible');                          
-  }, 1500);  
+    startBtn.classList.add('js_visible');
+  }, 2600);
 }
 
-function gameOverCounter() {   
-  if(unMatched === 10) {  // game over count !!   
-    gameOver();                 
-  }                                                             
+function gameOverCounter() {
+  if(unMatched === 10) {  
+    gameOver();
+  }    
 }
 
 function disableCards() {
   cards.forEach(card => {                       
     card.removeEventListener('click', flipCard);
   });
-} // GAME CLEAR. GAME OVER.時に クリックを解除!
+} 
 
 function shuffleCards() {
   const number = [];
@@ -139,34 +178,32 @@ function shuffleCards() {
     // console.log(orderNumber);
   });
 }
-shuffleCards();
 
+function detectMenubarStatus() {
+  if(mobile) {
+    if(innerHeight > defaultHeight) { 
+      menubar = false;
+      startBtn.classList.add('menubarHidden');
+    } else if(innerHeight === defaultHeight) {
+      menubar = true;
+      startBtn.classList.remove('menubarHidden');
+    }	
+  } else { startBtn.classList.add('menubarHidden')}
+} detectMenubarStatus();
+
+window.addEventListener('resize', () => {
+  portrait = window.matchMedia('(orientation: portrait)').matches;
+  orientation = portrait ? 'portrait' : 'landscape';
+  if(orientation === lastOrientation) { detectMenubarStatus()}
+  if(orientation !== lastOrientation && menubar) {
+    defaultHeight = innerHeight;
+    detectMenubarStatus();
+  }
+  lastOrientation = orientation;
+});
 
 //---------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
